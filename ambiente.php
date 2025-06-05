02edb2462f30891e5deb03d02e6c6e7a0f50ac90
ameri28 <santanaamerica39@gmail.com>
2025-06-05 11:31:43 -0600

    Add files via upload

---
+++ ambiente.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Concientización Ambiental</title>
+</head>
+<body>
+    <header>
+        <h2>Concientización Ambiental</h2>
+        <br><br/>
+        <p>filosofía de vida que se preocupa por el medioambiente y lo protege con el fin de conservarlo y de garantizar su equilibrio presente y futuro<p/>
+
+    </header>
+
+    <main>
+        <h2>Bienvenidos</h2>
+        <p>Este sitio busca promover el reciclaje y la limpieza en nuestra comunidad educativa.</p>
+        <img src="" alt="Reciclaje" width="300">
+        <h1>por que buscamos concientizar?</h1>
+        <p>Implica educar a las personas sobre los posibles peligros y las medidas que pueden tomar para evitar accidentes o daños a sí mismos y a otros.</p>
+    </main>
+
+    <footer>
+        <p>plantel cecyte aula externa ayotitlan.</p>
+    </footer>
+</body>
+</html>
